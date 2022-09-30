ea9f42e9fdc32482e443ef7feab73edc85cfd636
ImaneSaber2022 <saberimane663@gmail.com>
2022-09-30 17:56:19 +0100

    updatePlayiingVideo

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {StyleSheet} from 'react-native';
 import React from 'react';
 import 'react-native-gesture-handler';
 import Navigation from './Navigation';
+import PlayingVideo from './screens/PlayingVideo';
 
 const App = () => {
   return <Navigation />;
---
+++ screens/PlayingVideo.js
@@ -0,0 +1,27 @@
+// import {StyleSheet, Text, View} from 'react-native';
+// import React from 'react';
+// import Video from 'react-native-video';
+// const PlayingVideo = () => {
+//   return (
+//     <View>
+//       <Video
+//        source={{ url: 'http://link-to-your-video.mp4'}}
+//         paused={false} 
+//         style={styles.backgroundVideo} 
+//         repeat={true} 
+//       />
+//     </View>
+//   );
+// };
+
+// export default PlayingVideo;
+
+// const styles = StyleSheet.create({
+//     backgroundVideo: {
+//         position: 'absolute',
+//         top: 0,
+//         left: 0,
+//         bottom: 0,
+//         right: 0,
+//       },
+// });
